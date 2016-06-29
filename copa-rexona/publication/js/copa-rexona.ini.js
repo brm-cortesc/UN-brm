f34bf5f54b6c51527cc1586a51ad3349350e6df7
brm-barreraj <barreraj@iMac-de-Brm.local>
2016-06-29 12:08:31 -0500

    add XSF js

--- copa-rexona/publication/js/copa-rexona.ini.js
+++ copa-rexona/publication/js/copa-rexona.ini.js
@@ -29,6 +29,17 @@ loop={
 
 
 jQuery(document).ready(function($) {
+
+/* Oculta sección femenino y muestra masculino*/
+  jQuery("#btnMasculino").click(function(event) {
+    jQuery(".genero-F").hide(900);
+    jQuery(".genero-M").show(900);
+  });
+/* Oculta sección masculino y muestra femenino*/
+  jQuery("#btnFemenino").click(function(event) {
+    jQuery(".genero-M").hide(900);
+    jQuery(".genero-F").show(900);
+  });
   
 /*initialize sparks animation for desktop only*/
   if( screenWidth > 768 ){
@@ -108,5 +119,32 @@ var galeriaBtn = $('.selector-interna .galeria a'),
     }
 
 
+    /*Funcion para prevenir frame jacking*/
+
+    /*=====================================================*/
+    var __pcja_style = document.createElement('style');
+    __pcja_style.type = 'text/css';
+    __pcja_style.id = 'bfbs_cja';
+    var __pcja_css = 'html{ display:none !important; }';
+    if (__pcja_style.styleSheet){
+      __pcja_style.styleSheet.cssText = __pcja_css;
+    }else{
+      __pcja_style.appendChild(document.createTextNode(__pcja_css));
+    }
+
+    document.getElementsByTagName("head")[0].appendChild(__pcja_style);
+    if( self === top ){
+      var __bfbs_cja = document.getElementById( 'bfbs_cja' );
+      __bfbs_cja.parentNode.removeChild( document.getElementById( 'bfbs_cja' ) );
+    }else{
+      top.location = self.location;
+    }
+
+    /*Función para verificar el dominio XSF*/
+    try {
+      if (top.location.hostname != self.location.hostname) throw 1;
+    } catch (e) {
+      top.location.href = self.location.href;
+    }
 
 });
